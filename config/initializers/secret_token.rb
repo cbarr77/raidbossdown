39436027e96a0813c7097a460b0ab76ac48b98da
Benjamin <benjaminbarr77@benjaminbarr77-Inspiron-530.(none)>
2014-03-17 17:44:47 -0700

    Created rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raidbossdown::Application.config.secret_key_base = 'ad1d4e10dc97b2c1c12f2359c4e41bb98e4155f1cd73d4fcb4b0e78af11514c951286c03633cea27e4a65b3b3c0e325c7217979ee78bc7a241317bf415021df7'
